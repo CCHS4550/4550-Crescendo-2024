83ca21afc4e9c986ac616e3dfe58e1528c0a6e0b
ojlennon <45679125+ojlennon@users.noreply.github.com>
2024-01-30 09:41:57 -0700

    removed some imports

--- src/main/java/frc/controlschemes/CharacterizingScheme.java
+++ src/main/java/frc/controlschemes/CharacterizingScheme.java
@@ -1,21 +1,10 @@
 package frc.controlschemes;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.helpers.ControlScheme;
-import frc.helpers.OI;
 import frc.maps.ControlMap;
-import frc.maps.RobotMap;
 import frc.robot.subsystems.SwerveDrive;
 
 public class CharacterizingScheme implements ControlScheme{
